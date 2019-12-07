c610d68b95794cf047b953d0ee3a2a9de8205c6a
Gabriel27109812362 <gabriel.opolski98362@gmail.com>
2019-12-07 20:10:40 +0100

    add client entities

--- src/models/client.ts
+++ src/models/client.ts
@@ -1,4 +1,21 @@
-import {PrimaryGeneratedColumn, Column} from 'typeorm';
+import {PrimaryGeneratedColumn, Column, OneToOne, JoinColumn, Entity} from 'typeorm';
+import {ClientPersonalData} from './clientPersonalData';
 
+@Entity()
 export class Client {
+
+    @PrimaryGeneratedColumn('increment')
+    idClient: number;
+
+    @Column({type: 'varchar', nullable: false})
+    nick: string;
+
+    @Column({type: 'varchar', nullable: false})
+    password: string;
+
+    @Column({type: 'date', nullable: false})
+    registerDate: Date;
+
+    @OneToOne(type => ClientPersonalData, clientPersonalData => clientPersonalData.client)
+    clientPersonalData: ClientPersonalData;
 }
