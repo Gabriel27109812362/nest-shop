caadd4a93db58c3ad684f508c63764fa3ef81bad
Gabriel27109812362 <gabriel.opolski98362@gmail.com>
2019-12-08 22:33:00 +0100

    Update client entity on additional relationships to transactions, invoices and orders

--- src/models/client.ts
+++ src/models/client.ts
@@ -1,11 +1,12 @@
-import {PrimaryGeneratedColumn, Column, OneToOne, JoinColumn, Entity} from 'typeorm';
+import {PrimaryGeneratedColumn, Column, Entity, OneToOne, OneToMany} from 'typeorm';
 import {ClientPersonalData} from './clientPersonalData';
+import {Transaction} from './transaction';
+import {Invoice} from './invoice';
+import {Order} from './order';
 
 @Entity()
 export class Client {
-
-    @PrimaryGeneratedColumn('increment')
-    idClient: number;
+    @PrimaryGeneratedColumn('increment') idClient: number;
 
     @Column({type: 'varchar', nullable: false})
     nick: string;
@@ -18,4 +19,13 @@ export class Client {
 
     @OneToOne(type => ClientPersonalData, clientPersonalData => clientPersonalData.client)
     clientPersonalData: ClientPersonalData;
+
+    @OneToMany(type => Transaction, transaction => transaction.client)
+    transactions: Transaction[];
+
+    @OneToMany(type => Invoice, invoice => invoice.client)
+    invoices: Invoice[];
+
+    @OneToMany(type => Order, order => order.client)
+    orders: Order[];
 }
