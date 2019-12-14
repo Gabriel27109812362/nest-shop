cc45ef8e7049a9be0ee492ddf1e08945e279929e
Gabriel27109812362 <gabriel.opolski98362@gmail.com>
2019-12-14 16:38:41 +0100

    Add many to many relationship with producers and categories, and many to one with storeHouse and vat entities also delete price column

--- src/models/product.ts
+++ src/models/product.ts
@@ -1,7 +1,11 @@
-import { Column, Entity, JoinColumn, ManyToMany, ManyToOne, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
+import { Column, Entity, JoinColumn, JoinTable, ManyToMany, ManyToOne, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
 import { OrderDetails } from './orderDetails';
 import { Order } from './order';
 import { Vat } from './vat';
+import { Category } from './category';
+import { StoreHouse } from './storeHouse';
+import { Producer } from './producer';
+import { ProductProducer } from './productProducer';
 
 @Entity('product')
 export class Product {
@@ -12,23 +16,57 @@ export class Product {
   @Column({ type: 'varchar' })
   name: string;
 
-  @Column({ type: 'numeric' })
-  price: number;
-
   @Column({ type: 'integer' })
   magazineState: number;
 
   @Column({ type: 'varchar' })
   unitOfMeasure: string;
 
+  @ManyToOne(type => Vat, vat => vat.products)
+  @JoinColumn({ name: 'idVat' })
+  vat: Vat;
+
+  @ManyToOne(type => StoreHouse, store => store.products)
+  @JoinColumn({ name: 'idStoreHouse' })
+  storeHouse: StoreHouse;
+// relationship products -> orders via orderDetails junction table (bi-directional)
   @OneToMany(type => OrderDetails, orderDetails => orderDetails.product)
   orderDetails: OrderDetails[];
 
   @ManyToMany(type => Order, order => order.products)
   orders: Order;
+//
+// relationship products -> producers via productProducer junction table (bi-directional)
+  @OneToMany(type => ProductProducer, productProducer => productProducer.product)
+  productProducers: ProductProducer[];
 
-  @ManyToOne(type => Vat, vat => vat.products)
-  @JoinColumn({ name: 'idVat' })
-  vat: Vat;
-
+  @ManyToMany(type => Producer, producer => producer.products)
+  @JoinTable({
+    name: 'product_producer',
+    joinColumn: {
+      name: 'idProduct',
+      referencedColumnName: 'idProduct',
+    },
+    inverseJoinColumn: {
+      name: 'idProducer',
+      referencedColumnName: 'idProducer',
+    },
+  })
+  producers: Producer[];
+//
+// relationship products -> categories (bi-directional)
+  @ManyToMany(type => Category, category => category.products)
+  @JoinTable({
+    name: 'product_category',
+    joinColumn: {
+      name: 'idProduct',
+      referencedColumnName: 'idProduct',
+    },
+    inverseJoinColumn: {
+      name: 'idCategory',
+      referencedColumnName: 'idCategory',
+    },
+  })
+  categories: Category[];
+//
 }
